01501a628d5658365f35b3b9844c15ec6844b96d
TrioDW <nozerose@naver.com>
2024-12-07 21:08:56 +0900

    [CHORE] : DB 경로 수정

--- app/database.py
+++ app/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:dbterm!2024@211.213.193.67:5432/dbterm2024"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
